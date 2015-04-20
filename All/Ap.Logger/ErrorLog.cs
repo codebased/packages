a6d5185cf5b5c8c239b53cbfdf115e16307da9b1
Amit Malhotra <amit.malhotra2@cba.com.au>
2015-04-20 11:12:25 +1000

    Introducing Elmah Logger wrapper

---
+++ All/Ap.Logger/ErrorLog.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+using System.Web;
+using Elmah;
+
+namespace Ap.Logger
+{
+    public static class ErrorLog
+    {
+        /// <summary>
+        /// Log error to Elmah
+        /// </summary>
+        public static void LogError(string message, Exception innerException = null)
+        {
+            try
+            {
+                var exception = innerException;
+
+                // log error to Elmah
+                if (innerException != null)
+                {
+                    // log exception with contextual information that's visible when 
+                    // clicking on the error in the Elmah log
+                    exception = new Exception(message, innerException);
+
+                    // Elmah.ErrorLog.GetDefault(null).Log(new Elmah.Error(e));
+                }
+
+                ErrorSignal.FromCurrentContext().Raise(exception, HttpContext.Current);
+            }
+            catch
+            {
+                // uh oh! just keep going
+            }
+        }
+    }
+}
