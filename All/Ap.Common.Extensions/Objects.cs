3de9af07d35bd7f3d4195151fe027a01d136e7dc
Amit Malhotra <amit.malhotra2@cba.com.au>
2015-04-20 14:35:01 +1000

    Introducing ToNameValueCollection extension

--- All/Ap.Common.Extensions/Objects.cs
+++ All/Ap.Common.Extensions/Objects.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Specialized;
 
 namespace Ap.Common.Extensions
 {
     public static class Objects
     {
+        public static NameValueCollection ToNameValueCollection(this object item)
+        {
+            if (!item.IsNotNull()) return null;
+
+            var propNames = new NameValueCollection();
+
+            foreach (var propertyInfo in item.GetType().GetProperties())
+            {
+                if (!propertyInfo.CanRead) continue;
+
+                var propertyName = propertyInfo.Name;
+                var propertyValue = propertyInfo.GetValue(item, null);
+                propNames.Add(propertyName, (propertyValue ?? string.Empty).ToString());
+            }
+
+            return propNames;
+        }
     }
 }
