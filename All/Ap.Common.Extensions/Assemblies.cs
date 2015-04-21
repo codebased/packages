d91227be20a839935e5f5750bf94381e216f1ca8
Amit Malhotra <amit.malhotra2@cba.com.au>
2015-04-21 10:52:23 +1000

    GetResourceText From Assembly Extension

---
+++ All/Ap.Common.Extensions/Assemblies.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Ap.Common.Extensions
+{
+    public static class Assemblies
+    {
+        public static string GetResourceText(this Assembly source, string resourceName)
+        {
+            string result;
+            using (var stream = source.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) return string.Empty;
+                var reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+}
