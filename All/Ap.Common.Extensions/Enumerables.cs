a8e4d6aaff6f98069a346a38a086174631f87223
Amit Malhotra <amit.malhotra2@cba.com.au>
2015-04-21 10:59:03 +1000

    IdentityBaseMapper class for base entity framework

--- All/Ap.Common.Extensions/Enumerables.cs
+++ All/Ap.Common.Extensions/Enumerables.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ap.Common.Extensions
+﻿namespace Ap.Common.Extensions
 {
     class Enumerables
     {
---
+++ All/Ap.EntityFramework/IdentityBaseMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Ap.EntityFramework
+{
+    public class IdentityBaseMapper<T> : EntityTypeConfiguration<T> where T : IdentityBase
+    {
+        public IdentityBaseMapper()
+        {
+            HasKey(i => i.Id);
+            Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                .IsRequired();
+
+            Property(i => i.UniqueIdentifier)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+        }
+    }
+}
