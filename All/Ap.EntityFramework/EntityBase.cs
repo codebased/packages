c4adcfc6f0c9d821ea9090f50807ae6e1744fae1
Amit Malhotra <amit.malhotra2@cba.com.au>
2015-04-21 11:05:04 +1000

    EntityBaseMapper class for base entity framework

---
+++ All/Ap.EntityFramework/EntityBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ap.EntityFramework
+{
+    public abstract class EntityBase : IdentityBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the EntityBase class.
+        /// </summary>
+        public EntityBase()
+        {
+            DateModified = DateTime.UtcNow;
+            DateCreated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Gets or sets the date when the entity was created.
+        /// </summary>
+        public DateTime? DateCreated
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the date when the entity was updated.
+        /// </summary>
+        public DateTime? DateModified
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ All/Ap.EntityFramework/EntityBaseMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Ap.EntityFramework
+{
+    public class EntityBaseMapper<T> :
+         IdentityBaseMapper<T> where T : EntityBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the EntityBaseMapper class.
+        /// </summary>
+        public EntityBaseMapper()
+        {
+            Property(i => i.DateModified).HasColumnType("smalldatetime")
+                .IsRequired();
+
+            Property(i => i.DateCreated).HasColumnType("smalldatetime")
+                .IsRequired();
+        }
+    }
+}
